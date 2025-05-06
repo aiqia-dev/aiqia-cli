fc7f4b616e3f8b705f5de4b4565d636223b5341c
Felipe Campos <felipecamposdev@gmail.com>
2025-05-06 17:39:01 -0300

    :bug: fix zod scheme in example

--- templates/react/src/features/example/schemas/example.schema.ts
+++ templates/react/src/features/example/schemas/example.schema.ts
@@ -1,12 +1,11 @@
 import { z } from 'zod';
 
 export const exampleSchema = z.object({
-  name: z.string().nonempty({ message: 'Name is required' }),
-  phone: z.string().nonempty({ message: 'Phone is required' }),
-  email: z.string().email({ message: 'Invalid email format' }).nonempty({ message: 'Email is required' }),
-  licenseNumber: z.string().nonempty({ message: 'License number is required' }),
-  active: z.boolean().nonempty({ message: 'Active status is required' })
+  name: z.string().min(1, { message: 'Name is required' }),
+  phone: z.string().min(1, { message: 'Phone is required' }),
+  email: z.string().email({ message: 'Invalid email format' }),
+  licenseNumber: z.string().min(1, { message: 'License number is required' }),
+  active: z.boolean(),
 });
 
 export type ExampleFormData = z.infer<typeof exampleSchema>;
-
